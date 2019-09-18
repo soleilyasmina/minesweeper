b43a320313fb47254ca53b1ff796863fb8ad6e63
soleilyasmina <mounlasolomon@gmail.com>
2019-09-17 22:40:11 -0400

    added bombs

--- src/services/board.js
+++ src/services/board.js
@@ -1,4 +1,49 @@
-const generateBoard = (size) => {
+const provideBombs = (board) => {
+  let currentBombs = 0;
+  for (let i = 0; i < 10; i++) {
+    while (currentBombs === i) {
+      const y = Math.floor(Math.random() * board.length);
+      const x = Math.floor(Math.random() * board.length);
+      if (!board[y][x].isBomb) {
+        board[y][x].isBomb = true;
+        currentBombs += 1;
+      }
+    }
+  }
+  return board;
+}
+
+const isCoordinate = (y, x, size) => {
+  return y >= 0 && x >= 0 && y < size && x < size;
+}
+
+const getNeighbors = (y, x, size) => {
+  const neighbors = []; 
+  for (let j = -1; j <= 1; j++) {
+    for (let i = -1; i <= 1; i++) {
+      if (isCoordinate(y + j, x + i, size)) {
+        neighbors.push({
+          y: y + j,
+          x: x + i
+        });
+      }
+    }
+  }
+  return neighbors;
+}
+
+const declareNeighboringBombs = (board) => {
+  const bombs = board.flat(1).filter((box) => box.isBomb);
+  bombs.forEach((bomb) => {
+    const neighbors = getNeighbors(bomb.y, bomb.x, board.length);
+    neighbors.forEach(({ y, x }) => {
+      board[y][x].neighboringBombs += 1;
+    });
+  });
+  return board;
+}
+
+export const generateBoard = (size) => {
   const board = [];
   for (let y = 0; y < size; y++) {
     const row = [];
@@ -12,7 +57,7 @@ const generateBoard = (size) => {
     }
     board.push(row);
   }
-  return board;
+  const boardWithBombs = provideBombs(board);
+  const boardWithNeighboringBombs = declareNeighboringBombs(boardWithBombs);
+  return boardWithNeighboringBombs;
 }
-
-console.log(generateBoard(5));
